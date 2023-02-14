3ae494a48ebb0fc7f60a185b542f7c8e82dc695e
MEGHANA NEKAR <meghananekar@MEGHANAs-MacBook-Air.local>
2023-02-14 14:55:09 +0530

    Task 4's change

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
+  cout << "Hello, Universe\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built, changed the file and run\n";
   return 0;
 }
