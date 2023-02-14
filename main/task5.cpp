791e18f88abbc417444206396df715f32736d1f9
MEGHANA NEKAR <meghananekar@MEGHANAs-MacBook-Air.local>
2023-02-14 15:17:52 +0530

    Task 5

---
+++ main/task5.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Universe\n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built for task 5 \n";
+  return 0;
+}
